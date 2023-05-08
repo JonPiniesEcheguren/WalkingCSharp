c29f3ec1e7b890aca1fdbe037f9d362cd38747ba
JonPiniesEcheguren <jon.pinies.ech@gmail.com>
2023-05-08 18:24:04 +0200

    main branch updated

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// There are 13 branches in this program. This main branch plus one branch per each of the 12 exercises explained in the ReadMe.md document
+// The name of the branches are Uni_N_ejer_M
